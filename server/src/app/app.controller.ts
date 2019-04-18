bcb468916d79ff17c7c7d98707091c820619e532
IBMTask20190418 <stanislovas.janonis+ibm@gmail.com>
2019-04-18 20:58:11 +0300

    add response caching

--- server/src/app/app.controller.ts
+++ server/src/app/app.controller.ts
@@ -1,7 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  UseInterceptors,
+  CacheInterceptor,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller('api')
+@UseInterceptors(CacheInterceptor)
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Module, CacheModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AxiosProvider } from '../providers/axios';
 
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.register({
+      ttl: 5, // seconds
+      max: 1,
+    }),
+  ],
   controllers: [AppController],
   providers: [AxiosProvider, AppService],
 })
