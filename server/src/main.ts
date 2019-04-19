074dc4f625671b6b63fb2b070ed08406c2cd2aa7
IBMTask20190418 <stanislovas.janonis+ibm@gmail.com>
2019-04-19 20:57:15 +0300

    Add swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  // Setup docs generation
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('LRT Tiesiogiai')
+    .setDescription('LRT Tiesiogiai API')
+    .setVersion('1.0')
+    .addTag('lrt')
+    .setBasePath('api')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3001);
 }
 bootstrap();
